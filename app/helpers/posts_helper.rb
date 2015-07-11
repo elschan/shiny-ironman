c5dd6ee567240251b097c5fea8eb396acaf10bb6
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-11 16:15:19 -0700

    post overview shows username

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,4 @@
 module PostsHelper
-  def get_username(member_id)
-    @member = Member.find(member_id)
-    username = @member.username
-  end
-
   def post_time_ago(post)
     post.created_at
   end
