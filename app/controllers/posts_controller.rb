a2ad38cf852e939a524903c3720aa46dc8a9aaf0
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-11 10:14:55 -0700

    small changes to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,38 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    # automatically serves /posts and @posts is available to that page
+  end
 
+  def new
+    @post = Post.new
+    # automatically serves /posts/new and @post is available to that page
+    # /posts/new is a form
   end
 
   def create
-
+    # catches the post request to posts/new
+    # save a new post using params hash
+    # redirect to posts index
   end
 
-  def delete
-
+  def edit
+    @post = Post.find(params[:id])
+    # serve /posts/ID/edit and put a form on the page
   end
+
+  def update
+
+    # save post params
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+  end
+
+  protected
+
+  def post_params
+    params.require(:title).permit()
 
 end
