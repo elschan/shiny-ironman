902f2b9b568e11756652f40d41e276f029df8eee
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-11 16:53:07 -0700

    fix migration problems

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711203742) do
+ActiveRecord::Schema.define(version: 20150710022839) do
 
   create_table "comments", force: true do |t|
     t.integer  "parent_comment_id"
     t.integer  "post_id"
+    t.integer  "member_id"
     t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "member_id"
   end
 
   create_table "invites", force: true do |t|
@@ -32,12 +32,10 @@ ActiveRecord::Schema.define(version: 20150711203742) do
 
   create_table "members", force: true do |t|
     t.string   "username"
-    t.string   "password_digest"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer  "invite_count"
     t.integer  "reputation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
