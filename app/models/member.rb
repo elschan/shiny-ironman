a4401ca62b35ebd8335610006d34da9ae6334bea
elsieC <elschan90@gmail.com>
2015-07-11 22:37:25 +0000

    added confirmation text input

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   # validates :email, presence: true, uniqueness: true
   # validates :username, presence: true, uniqueness: true, length: { in: 4..15 }
   # validates :password_digest, presence: true
