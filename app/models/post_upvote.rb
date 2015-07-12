9a3f4d998536a540dbf4b7ec666b4ca0511eaef9
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-11 23:33:19 -0700

    add elisas changes to username in login

--- app/models/post_upvote.rb
+++ app/models/post_upvote.rb
@@ -1,3 +1,6 @@
 class PostUpvote < Upvote
   belongs_to :post, foreign_key: 'parent_id'
+
+  # TODO: refactor and put in the Upvote model
+  # after save, update upvote count in the correct table
 end
--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,3 +1,10 @@
 class Upvote < ActiveRecord::Base
   belongs_to :member
+
+  # before save, validate that the member_id/parent_id/type is unique.
+  # This is kind of hair-brained and might not work.
+  validates :member_id, uniqueness: {scope: [:parent_id, :type]}
+
+  # before_save :update_upvote_total
+  
 end
