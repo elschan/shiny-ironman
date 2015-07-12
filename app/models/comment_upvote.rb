d79189f37c14f8bfc4d335d21d287c73bb137424
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-12 00:11:25 -0700

    added upvotes controller

--- app/models/comment_upvote.rb
+++ app/models/comment_upvote.rb
@@ -1,3 +1,5 @@
 class CommentUpvote < Upvote
   belongs_to :comment, foreign_key: 'parent_id'
+
+  # after_save :update_total_comment_upvotes
 end
--- app/models/post_upvote.rb
+++ app/models/post_upvote.rb
@@ -3,4 +3,11 @@ class PostUpvote < Upvote
 
   # TODO: refactor and put in the Upvote model
   # after save, update upvote count in the correct table
+  after_save :update_total_post_upvotes
+
+  protected
+
+  def update_total_post_upvotes
+    binding.pry
+  end
 end
