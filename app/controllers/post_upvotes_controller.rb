71a9643c8c34e191201e6ba1d21a70f2afed3acf
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-12 15:27:46 -0700

    delete upvote related models and controllers

--- app/controllers/post_upvotes_controller.rb
+++
@@ -1,6 +0,0 @@
-class PostUpvotesController < ApplicationController
-  def create
-    @post = Post.find(params[:id])
-    
-  end
-end
--- app/models/upvote.rb
+++
@@ -1,8 +0,0 @@
-class Upvote < ActiveRecord::Base
-  belongs_to :member
-
-  # before save, validate that the member_id/parent_id/type is unique.
-  # This is kind of hair-brained and might not work.
-  validates :member_id, uniqueness: {scope: [:parent_id, :type]}
-  
-end
