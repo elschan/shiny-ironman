bd86d0f22fd40478f828bc6def58a6a9b3cd23b7
elsieC <elschan90@gmail.com>
2015-07-12 00:46:53 +0000

    addingsomelogins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ShinyIronman::Application.routes.draw do
+  devise_for :members
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
