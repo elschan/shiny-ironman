3826c9771ad1c85c971ba6bab4de8bcbe20c5665
elsieC <elschan90@gmail.com>
2015-07-12 17:36:38 +0000

    hide

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ ShinyIronman::Application.configure do
   domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: "mamahouhouapp@gmail.com",
-  password: ":fu:&shine",
+  user_name: "",
+  password: "",
   :enable_starttls_auto => true
 
   }
