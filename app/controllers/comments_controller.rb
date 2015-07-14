68a8dff660d8efa837ef1ebcf69364eba05a740b
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-13 23:31:40 -0700

    lots todo in comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,19 +9,26 @@ class CommentsController < ApplicationController
     @comment = Comment.new()
     @comment.member_id = current_member.id
 
+    # TODO is there a better way to do this post_id than a hidden_field (for replies)
+    # See _comments.html.erb
+    @comment.post_id = params[:post_id].to_i
+
     # If comment is a reply
     if params[:parent_id]
       @comment.parent_comment_id = params[:parent_id].to_i
       @comment.text = params[:text]
     else
-      @comment.post_id = params[:post_id].to_i
       @comment.text = params[:comment][:text]
     end
 
     if @comment.save
+      binding.pry
+      # TODO Fix this, it's broken and doesn't take params in this way.
       redirect_to post_path(@comment.post_id)
     else
-      errors[:base] << "There has been a problem"
+      # TODO how to I print the error from here? Comment save fails when the
+      # comment text is too dang short
+
       render post_path(@comment.post_id)
     end
 
