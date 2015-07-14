485e8bd20cf9e2606cadb3103c281e2c8969ff22
elsieC <elschan90@gmail.com>
2015-07-14 01:57:21 +0000

    commented

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,17 +1,17 @@
 module MembersHelper
 
 
-  def total_posts_posted
-    total_posts = Post.where("member_id = #{self.id}").count
-  end
+  # def total_posts_posted
+  #   total_posts = Post.where("member_id = #{self.id}").count
+  # end
 
-  def total_upvotes_earned
-    total_upvotes = 0
-     Post.where("member_id = #{self.id}").each do |post|
-      total_upvotes += post.get_upvotes.size
-    end
-    return total_upvotes
-  end
+  # def total_upvotes_earned
+  #   total_upvotes = 0
+  #    Post.where("member_id = #{self.id}").each do |post|
+  #     total_upvotes += post.get_upvotes.size
+  #   end
+  #   return total_upvotes
+  # end
 
   
 end
