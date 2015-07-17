b986f9cd1463e29168d85d7eb0fb4bc8639c28c0
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-07-17 16:32:29 -0700

    removes unnecessary custom js

--- app/assets/javascripts/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.avatar').on('click', function(){
-    $(this).toggleClass('open');
-  });
-  $('.browse').on('click', function(){
-    $(this).toggleClass('open');
-  });
-});
