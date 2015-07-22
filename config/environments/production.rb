589b8f6934e8ae397c203e0ec81ae72efb30ff74
elsieC <elschan90@gmail.com>
2015-07-21 19:39:26 -0700

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,17 @@ config.action_mailer.default_url_options = { :host => 'theshineproject.heroku.co
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+    config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["shine_email"],
+  password: ENV["shine_pass"],
+  :enable_starttls_auto => true
+
+  }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
