b72fa2365d72834417ebd3e7ec7fca83c9d99995
elsieC <elschan90@gmail.com>
2015-07-22 06:33:08 -0700

    postgres change

--- Gemfile
+++ Gemfile
@@ -2,12 +2,12 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
-
+gem 'sqlite3'
 # Use sqlite3 as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
-
+gem 'taps'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
@@ -46,9 +46,7 @@ end
 
 gem 'rails_12factor', group: :production
 
-group :development do
-  gem 'sqlite3'
-end
+
 
 ruby "2.2.1"
 
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -14,10 +14,10 @@ class MembersController < ApplicationController
 
   def show
     @member = Member.find(params[:id])
-    @posts = Post.where("member_id = #{@member.id}")
-    @coffees_invited_to = Coffeemeet.where("invitee_id = #{@member.id}").reverse
-    @coffees_waiting_for = Coffeemeet.where("inviter_id = #{@member.id}").reverse
-    @coffees = Coffeemeet.where("invitee_id or inviter_id = #{@member.id}").reverse
+    @posts = Post.where("member_id = ?",@member.id)
+    @coffees_invited_to = Coffeemeet.where("invitee_id = ?", @member.id).reverse
+    @coffees_waiting_for = Coffeemeet.where("inviter_id = ?", @member.id).reverse
+    @coffees = Coffeemeet.where("invitee_id = ? or inviter_id = ?",@member.id, @member.id).reverse
     @coffee = Coffeemeet.new
   end
 
