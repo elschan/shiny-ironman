f02b80ac1ec008cb6c78f3e7c2f2c9eef82d1d7f
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-08-02 14:07:29 -0700

    can create a post from my profile

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -19,6 +19,7 @@ class MembersController < ApplicationController
     @coffees_waiting_for = Coffeemeet.where("inviter_id = ?", @member.id).reverse
     @coffees = Coffeemeet.where("invitee_id = ? or inviter_id = ?",@member.id, @member.id).reverse
     @coffee = Coffeemeet.new
+    @post = Post.new
   end
 
   def index
