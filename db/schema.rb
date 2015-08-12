6d54158884bea36f241e3d1ce93ff0e8c334b678
Jessica Thomas <jess.rae.thomas@gmail.com>
2015-08-12 00:16:09 -0700

    bundled and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805014424) do
+ActiveRecord::Schema.define(version: 20150808032114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20150805014424) do
     t.boolean  "seen",       default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "coffee_id"
   end
 
   create_table "posts", force: true do |t|
